2cf9b41ecee6407ca809a2039965d85c7ac17eec
Thomas <thomasmarchand63@gmail.com>
2024-07-07 18:01:26 +0200

    :sparkles: added hashed password field, fix validate field what not working at all

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -1,12 +1,30 @@
+import { hash } from "argon2";
+import { IsEmail, IsStrongPassword, Length } from "class-validator";
 import { Field, InputType, ObjectType } from "type-graphql";
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  BaseEntity,
+  BeforeInsert,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 
 @Entity()
 @ObjectType()
 export default class User extends BaseEntity {
+  password: string;
+
+  @BeforeInsert()
+  async hasPassword() {
+    this.hashedPassword = await hash(this.password);
+  }
+
   @Field()
   @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @Column()
+  hashedPassword: string;
 
   @Field()
   @Column()
@@ -24,12 +42,19 @@ export default class User extends BaseEntity {
 @InputType()
 export class CreateUserInput {
   @Field()
+  @IsEmail()
   email: string;
 
+  @Field()
+  @IsStrongPassword()
+  password: string;
+
   @Field({ nullable: true })
+  @Length(2, 30, { message: "Le prénom doit faire entre 2 et 30 caractères" })
   first_name: string;
 
   @Field({ nullable: true })
+  @Length(2, 30, { message: "Le nom doit faire entre 2 et 30 caractères" })
   last_name: string;
 }
 
